2d2144ba2257c67fb850595bca76ed4ce3b73aeb
majid <majid.asgari.cs@gmail.com>
2023-09-24 11:55:25 +0330

    Add compile options to cmakelist and read value of them in main.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,10 +1,28 @@
 #include <Speaker.h>
+#include <stdio.h>
+#include <iostream>
 
 using namespace std;
 using namespace Hello;
 
-int main(int argc, char *argv[]) {
-  Speaker* speaker = new Speaker();
+#define MAJID "majid"
 
-  speaker->sayHello();  
+int main(int argc, char *argv[]) {
+//   Speaker* speaker = new Speaker();
+
+//   speaker->sayHello();  
+
+  std::cout<<"FOO:"<<__FOO__<<" BAR"<<__BAR__<<" __AA__:"<<__AA__<<"\n";
+
+// #if(SEARCH==MAJID)
+//   std::cout<<"FOO is define:"<<FOO<<"\n";
+// #endif
+
+if(std::string(__FOO__) == MAJID){
+  std::cout<<"FOO is define:"<<__FOO__<<"\n";
+}
+
+if(atoi(__BAR__) == 100){
+  std::cout<<"BAR is define:"<<__BAR__<<"\n";
+}
 }
